1dd3bd7d4ec1c79f5d2963841065787c2afa34af
ingbin-git <eeeingbin@gmail.com>
2023-07-13 16:27:32 +0900

    mission01에 대한 힌트 코드 - querySelector - addEventLisener

--- client/main.js
+++ client/main.js
@@ -5,3 +5,18 @@
 // const a = 10;
 
 // const b = 10;
+
+const input = document.querySelector('#todo');
+
+let value = input.value;
+
+input.addEventListener('input', () => {
+  value = input.value;
+  // console.log(value);
+  console.log(value === 'hello');
+});
+
+// console.log(input.value);
+
+// input.classList.add('is-active');
+// input.classList.remove('is-active');
