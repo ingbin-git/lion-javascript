7b743e5046effced48d1a77143bf4d63ba0c9caa
ingbin-git <eeeingbin@gmail.com>
2023-07-24 11:34:37 +0900

    주접 생성기 코드 작성 완료

---
+++ client/lib/dom/showAlert.js
@@ -0,0 +1,16 @@
+import { addClass, removeClass } from './css.js';
+import { getNode } from './getNode.js';
+
+export function showAlert(node, text = '에러입니다!', timeout = 1000) {
+  if (typeof node === 'string') {
+    node = getNode(node);
+  }
+
+  node.textContent = text;
+
+  // 비동기
+  addClass(node, 'is-active');
+  setTimeout(() => {
+    removeClass(node, 'is-active');
+  }, timeout);
+}
