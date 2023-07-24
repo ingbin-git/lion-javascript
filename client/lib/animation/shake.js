51b175423a0436e2d84cad97939fc82b3fca8bf6
ingbin-git <eeeingbin@gmail.com>
2023-07-24 12:32:24 +0900

    shake 파일 /* global gsap */ 주석 추가

--- client/lib/animation/shake.js
+++ client/lib/animation/shake.js
@@ -1,11 +1,9 @@
-
-    /* global gsap */
-
-    export const shake = gsap.to('form',{
-      duration:0.1,
-      x:-10,
-      repeat:5,
-      yoyo:true,
-      clearProp:'x',
-      paused:true
-    })
+/* global gsap */
+export const shake = gsap.to('form', {
+  duration: 0.1,
+  x: -10,
+  repeat: 5,
+  yoyo: true,
+  clearProp: 'x',
+  paused: true,
+});
