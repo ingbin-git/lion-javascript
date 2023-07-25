a711534bac56c5d4094646024f2cb3487c40b101
ingbin-git <eeeingbin@gmail.com>
2023-07-25 18:03:15 +0900

    콜백 지옥 관련 코드 학습

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,16 +1,46 @@
+import { getNode } from '../dom/index.js';
+
 function delay(callback, timeout = 1000) {
   setTimeout(callback, timeout);
 }
 
-console.log(1);
-
+// 콜백 지옥
 delay(() => {
-  console.log(2);
+  // console.log(1);
+  delay(() => {
+    // console.log(2);
+    delay(() => {
+      // console.log(3);
+    });
+  });
 });
 
-delay(() => {
-  console.log(3);
-},2000);
+const first = getNode('.first');
+const second = getNode('.second');
 
+// delay(() => {
+//   first.style.top = '-100px';
+//   second.style.top = '100px';
 
+//   delay(() => {
+//     first.style.transform = 'rotate(360deg)';
+//     second.style.transform = 'rotate(360deg)';
 
+//     delay(() => {
+//       first.style.top = '0';
+//       second.style.top = '0';
+//     });
+//   });
+// });
+
+// delayP 함수를 실행하면 리턴되는 값이 promise 객체입니다.
+function delayP() {
+  return new Promise((resolve, reject) => {
+    resolve('성공입니다!');
+  });
+}
+
+delayP().then((result) => {
+  console.log(result);
+});
+console.log(delayP());
