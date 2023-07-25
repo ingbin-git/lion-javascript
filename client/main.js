2990b58dc07e6758daea5439d826d2fdf2e9dc5a
ingbin-git <eeeingbin@gmail.com>
2023-07-25 18:02:57 +0900

    코드 추가

--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
 import { xhr } from './lib/index.js';
 
-xhr.get('https://jsonplaceholder.typicode.com/users', (res) => {
-  // console.log(res);
-});
+// xhr.get('https://jsonplaceholder.typicode.com/users', (res) => {
+//   console.log(res);
+//   xhr.get('server', () => {
+//     xhr.get('px', () => {});
+//   });
+// });
