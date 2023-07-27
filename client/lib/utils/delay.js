09078ee50ed33f66e56ab92d57923b40f09a2b0f
ingbinsee <ingbinsee@gmail.com>
2023-07-27 13:00:24 +0900

    delayP 함수 export

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -45,7 +45,7 @@ const defaultOptions = {
   errMessage: '알 수 없는 오류가 발생했습니다.',
 };
 
-function delayP(options) {
+export function delayP(options) {
   let config = { ...defaultOptions };
   if (typeof options === 'number') {
     config.timeout = options;
